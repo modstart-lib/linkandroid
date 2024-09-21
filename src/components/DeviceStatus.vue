5690c5bea82e86d392f2ba1c2184c56019e5fc31
git <git@git-scm.com>
2024-09-21 21:07:21 +0800

    translate change

--- src/components/DeviceStatus.vue
+++ src/components/DeviceStatus.vue
@@ -13,7 +13,7 @@ const props = defineProps<Props>()
          :class="{'bg-gray-400': props.status===EnumDeviceStatus.WAIT_CONNECTING, 'bg-green-500': props.status===EnumDeviceStatus.CONNECTED, 'bg-red-500': props.status===EnumDeviceStatus.DISCONNECTED}">
         <div class="w-2 h-2 rounded-full bg-white mr-2"></div>
         <div v-if="props.status===EnumDeviceStatus.WAIT_CONNECTING">
-            {{ $t('等待连接') }}
+            {{ $t('连接中') }}
         </div>
         <div v-else-if="props.status===EnumDeviceStatus.CONNECTED">
             {{ $t('已连接') }}
