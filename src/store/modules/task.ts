c0b77bb39dbfcaaf59cbb8a33ab33297320c40c5
ModStart <modstart@163.com>
2024-10-17 22:16:45 +0800

    task管理优化

--- src/store/modules/task.ts
+++ src/store/modules/task.ts
@@ -6,7 +6,11 @@ import {StringUtil} from "../../lib/util";
 
 export type TaskRecordStatus = 'queue' | 'running' | 'querying' | 'success' | 'fail' | 'delete'
 
+export type TaskRecordRunStatus = 'retry' | 'success'
+
 export type TaskRecordQueryStatus = 'unknown' | 'running' | 'success' | 'fail'
+
+export type TaskChangeType = 'running' | 'success' | 'fail'
 
 export type TaskRecord = {
     id: string;
@@ -16,18 +20,24 @@ export type TaskRecord = {
     bizId: string;
     bizParam: any;
     runStart: number;
+    runAfter: number;
     queryAfter: number;
     queryInterval: number;
     timeout: number;
 }
 
 export type TaskBiz = {
-    runFunc: (bizId: string, bizParam: any) => Promise<void>,
-    queryFunc?: (bizId: string, bizParam: any) => Promise<TaskRecordQueryStatus>,
+    runFunc: (bizId: string, bizParam: any) => Promise<TaskRecordRunStatus>,
     successFunc: (bizId: string, bizParam: any) => Promise<void>,
     failFunc: (bizId: string, msg: string, bizParam: any) => Promise<void>,
+    queryFunc?: (bizId: string, bizParam: any) => Promise<TaskRecordQueryStatus>,
+    restore?: () => Promise<void>,
 }
 
+const taskChangeListeners = [] as {
+    biz: string,
+    callback: (bizId: string, status: TaskChangeType) => void
+}[]
 let runNextTimer = null as any
 
 export const taskStore = defineStore("task", {
@@ -53,9 +63,13 @@ export const taskStore = defineStore("task", {
                     changed = true
                 }
             }
+            // console.log('task.records', JSON.stringify(this.records, null, 2))
             // queue
             for (const record of this.records) {
                 if (record.status !== 'queue') {
+                    continue
+                }
+                if (record.runAfter > Date.now()) {
                     continue
                 }
                 changed = true
@@ -63,17 +77,26 @@ export const taskStore = defineStore("task", {
                 record.runStart = Date.now()
                 this.bizMap[record.biz]
                     .runFunc(record.bizId, record.bizParam)
-                    .then(() => {
-                        if (this.bizMap[record.biz].queryFunc) {
-                            record.queryAfter = Date.now() + record.queryInterval
-                            record.status = 'querying'
+                    .then((status: TaskRecordRunStatus) => {
+                        if ('retry' === status) {
+                            record.status = 'queue'
+                            record.runStart = 0
+                            record.runAfter = Date.now() + 1000
                         } else {
-                            record.status = 'success'
+                            if (this.bizMap[record.biz].queryFunc) {
+                                record.queryAfter = Date.now() + record.queryInterval
+                                record.status = 'querying'
+                            } else {
+                                record.status = 'success'
+                            }
                         }
                     })
                     .catch((e) => {
                         record.status = 'fail'
                         record.msg = e.message
+                    })
+                    .finally(() => {
+                        this.fireChange(record, 'running')
                     })
             }
             // querying
@@ -81,6 +104,7 @@ export const taskStore = defineStore("task", {
                 if (record.status !== 'querying' && record.status !== 'running') {
                     continue
                 }
+                // console.log('querying', JSON.stringify(record, null, 2))
                 if (record.queryAfter > Date.now()) {
                     continue
                 }
@@ -132,6 +156,9 @@ export const taskStore = defineStore("task", {
                     })
                     .catch((e) => {
                     })
+                    .finally(() => {
+                        this.fireChange(record, 'success')
+                    })
             }
             // fail
             for (const record of this.records) {
@@ -149,6 +176,9 @@ export const taskStore = defineStore("task", {
                     })
                     .catch((e) => {
                     })
+                    .finally(() => {
+                        this.fireChange(record, 'fail')
+                    })
             }
             // delete
             this.records = this.records.filter(r => r.status !== 'delete')
@@ -157,7 +187,7 @@ export const taskStore = defineStore("task", {
                 this.sync().then()
             }
             // next run
-            console.log('run', changed, JSON.stringify(this.records))
+            // console.log('run', changed, JSON.stringify(this.records))
             if (this.records.length > 0) {
                 this._run(changed)
             }
@@ -177,6 +207,20 @@ export const taskStore = defineStore("task", {
         unregister(biz: string) {
             delete this.bizMap[biz]
         },
+        onChange(biz: string, callback: (bizId: string, type: TaskChangeType) => void) {
+            taskChangeListeners.push({biz, callback})
+        },
+        offChange(biz: string, callback: (bizId: string, type: TaskChangeType) => void) {
+            const index = taskChangeListeners.findIndex((v) => v.biz === biz && v.callback === callback)
+            taskChangeListeners.splice(index, 1)
+        },
+        fireChange(record: TaskRecord, type: TaskChangeType) {
+            taskChangeListeners.forEach((v) => {
+                if (v.biz === record.biz) {
+                    v.callback(record.bizId, type)
+                }
+            })
+        },
         async dispatch(biz: string, bizId: string, bizParam?: any, param?: object) {
             if (!this.bizMap[biz]) {
                 throw new Error('TaskBizNotFound')
@@ -184,15 +228,18 @@ export const taskStore = defineStore("task", {
             param = Object.assign({
                 timeout: 60 * 1000,
                 queryInterval: 1 * 1000,
+                status: 'queue',
+                runStart: 0,
             }, param)
             const taskRecord = {
                 id: `${biz}-${Date.now()}-${StringUtil.random(8)}`,
-                status: 'queue' as TaskRecordStatus,
+                status: param['status'],
                 msg: '',
                 biz,
                 bizId,
                 bizParam,
-                runStart: 0,
+                runStart: param['runStart'],
+                runAfter: 0,
                 queryAfter: 0,
                 queryInterval: param['queryInterval'],
                 timeout: param['timeout'],
