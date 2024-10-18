62357e6e8d3d565c3a80e86a332d8f600306fb93
ModStart <modstart@163.com>
2024-10-18 10:40:15 +0800

    task管理优化

--- src/store/modules/task.ts
+++ src/store/modules/task.ts
@@ -3,12 +3,13 @@ import store from "../index";
 import {toRaw} from "vue";
 import {cloneDeep} from "lodash-es";
 import {StringUtil} from "../../lib/util";
+import {mapError} from "../../lib/aigcpanel";
 
 export type TaskRecordStatus = 'queue' | 'running' | 'querying' | 'success' | 'fail' | 'delete'
 
 export type TaskRecordRunStatus = 'retry' | 'success'
 
-export type TaskRecordQueryStatus = 'unknown' | 'running' | 'success' | 'fail'
+export type TaskRecordQueryStatus = 'running' | 'success' | 'fail'
 
 export type TaskChangeType = 'running' | 'success' | 'fail'
 
@@ -19,18 +20,30 @@ export type TaskRecord = {
     biz: string;
     bizId: string;
     bizParam: any;
+    // 开始运行时间
     runStart: number;
+    // 是否正在调用 runFunc
+    runCalling: boolean;
+    // 超过 runAfter 才会执行，0表示是一个新任务，＞0表示是一个重试任务
     runAfter: number;
+    // 是否正在调用 queryFunc
+    queryCalling: boolean;
+    // 超过 queryAfter 才会查询
     queryAfter: number;
+    // 查询间隔
     queryInterval: number;
+    // 是否正在调用 successFunc
+    successCalling: boolean;
+    // 超时时间
     timeout: number;
 }
 
 export type TaskBiz = {
     runFunc: (bizId: string, bizParam: any) => Promise<TaskRecordRunStatus>,
-    successFunc: (bizId: string, bizParam: any) => Promise<void>,
-    failFunc: (bizId: string, msg: string, bizParam: any) => Promise<void>,
     queryFunc?: (bizId: string, bizParam: any) => Promise<TaskRecordQueryStatus>,
+    successFunc: (bizId: string, bizParam: any) => Promise<void>,
+    // 请确保 failFunc 不会抛出异常
+    failFunc: (bizId: string, msg: string, bizParam: any) => Promise<void>,
     restore?: () => Promise<void>,
 }
 
@@ -56,130 +69,132 @@ export const taskStore = defineStore("task", {
         },
         _runExecute() {
             let changed = false
-            for (const record of this.records) {
+            // error record
+            this.records.forEach((record) => {
                 if (!this.bizMap[record.biz]) {
                     record.status = 'fail'
                     record.msg = 'biz not found'
                     changed = true
                 }
-            }
+            })
             // console.log('task.records', JSON.stringify(this.records, null, 2))
             // queue
-            for (const record of this.records) {
-                if (record.status !== 'queue') {
-                    continue
-                }
-                if (record.runAfter > Date.now()) {
-                    continue
-                }
-                changed = true
-                record.status = 'running'
-                record.runStart = Date.now()
-                this.bizMap[record.biz]
-                    .runFunc(record.bizId, record.bizParam)
-                    .then((status: TaskRecordRunStatus) => {
-                        if ('retry' === status) {
-                            record.status = 'queue'
-                            record.runStart = 0
-                            record.runAfter = Date.now() + 1000
-                        } else {
-                            if (this.bizMap[record.biz].queryFunc) {
-                                record.queryAfter = Date.now() + record.queryInterval
-                                record.status = 'querying'
-                            } else {
-                                record.status = 'success'
-                            }
-                        }
-                    })
-                    .catch((e) => {
-                        record.status = 'fail'
-                        record.msg = e.message
-                    })
-                    .finally(() => {
-                        this.fireChange(record, 'running')
-                    })
-            }
-            // querying
-            for (const record of this.records) {
-                if (record.status !== 'querying' && record.status !== 'running') {
-                    continue
-                }
-                // console.log('querying', JSON.stringify(record, null, 2))
-                if (record.queryAfter <= 0 || record.queryAfter > Date.now()) {
-                    continue
-                }
-                const taskBiz = this.bizMap[record.biz]
-                taskBiz.queryFunc?.(record.bizId, record.bizParam)
-                    .then((status) => {
-                        switch (status) {
-                            case 'unknown':
-                            case 'running':
-                                record.queryAfter = Date.now() + record.queryInterval
-                                break
-                            case 'success':
-                                record.status = 'success'
-                                changed = true
-                                break
-                            case 'fail':
-                                record.status = 'fail'
-                                changed = true
-                                break
-                        }
-                    })
-                    .catch((e) => {
-                        record.status = 'fail'
-                        record.msg = e.message
-                        changed = true
-                    })
-            }
-            // expire
-            for (const record of this.records) {
-                if (record.status !== 'running' && record.status !== 'querying') {
-                    continue
-                }
-                if (Date.now() - record.runStart > record.timeout) {
-                    record.status = 'fail'
-                    record.msg = 'timeout'
+            this.records
+                .filter(r => r.status === 'queue')
+                .filter(r => r.runAfter <= Date.now() && !r.runCalling)
+                .forEach((record) => {
                     changed = true
-                }
-            }
+                    record.status = 'running'
+                    record.runStart = Date.now()
+                    record.runCalling = true
+                    this.bizMap[record.biz]
+                        .runFunc(record.bizId, record.bizParam)
+                        .then((status: TaskRecordRunStatus) => {
+                            if ('retry' === status) {
+                                record.status = 'queue'
+                                record.runStart = 0
+                                record.runAfter = Date.now() + 1000
+                            } else {
+                                if (!!this.bizMap[record.biz].queryFunc) {
+                                    record.queryAfter = Date.now() + record.queryInterval
+                                    record.status = 'querying'
+                                } else {
+                                    record.status = 'success'
+                                }
+                            }
+                        })
+                        .catch((e) => {
+                            record.status = 'fail'
+                            record.msg = mapError(e)
+                        })
+                        .finally(() => {
+                            record.runCalling = false
+                            this.fireChange(record, 'running')
+                        })
+                })
+            // querying
+            this.records
+                .filter(r => r.status === 'querying')
+                .filter(r => r.queryAfter <= Date.now() && !r.queryCalling)
+                .forEach((record) => {
+                    record.queryCalling = true
+                    const taskBiz = this.bizMap[record.biz]
+                    taskBiz.queryFunc?.(record.bizId, record.bizParam)
+                        .then((status) => {
+                            switch (status) {
+                                case 'running':
+                                    record.queryAfter = Date.now() + record.queryInterval
+                                    break
+                                case 'success':
+                                    record.status = 'success'
+                                    changed = true
+                                    break
+                                case 'fail':
+                                    record.status = 'fail'
+                                    changed = true
+                                    break
+                            }
+                        })
+                        .catch((e) => {
+                            record.status = 'fail'
+                            record.msg = mapError(e)
+                            changed = true
+                        })
+                        .finally(() => {
+                            record.queryCalling = false
+                        })
+                })
+            // expire
+            this.records
+                .filter(r => r.status === 'running' || r.status === 'querying')
+                .filter(r => Date.now() - r.runStart > r.timeout)
+                .forEach((record) => {
+                    record.status = 'fail'
+                    record.msg = mapError('ProcessTimeout')
+                    changed = true
+                })
             // success
-            for (const record of this.records) {
-                if (record.status !== 'success') {
-                    continue
-                }
-                changed = true
-                record.status = 'delete'
-                this.bizMap[record.biz]
-                    .successFunc(record.bizId, record.bizParam)
-                    .then(() => {
-                    })
-                    .catch((e) => {
-                    })
-                    .finally(() => {
-                        this.fireChange(record, 'success')
-                    })
-            }
+            this.records
+                .filter(r => r.status === 'success')
+                .filter(r => !r.successCalling)
+                .forEach((record) => {
+                    record.successCalling = true
+                    changed = true
+                    this.bizMap[record.biz]
+                        .successFunc(record.bizId, record.bizParam)
+                        .then(() => {
+                            record.status = 'delete'
+                        })
+                        .catch((e) => {
+                            record.status = 'fail'
+                            record.msg = mapError(e)
+                        })
+                        .finally(() => {
+                            if (record.status === 'delete') {
+                                this.fireChange(record, 'success')
+                            }
+                            record.successCalling = false
+                        })
+                })
             // fail
-            for (const record of this.records) {
-                if (record.status !== 'fail') {
-                    continue
-                }
-                changed = true
-                record.status = 'delete'
-                if (!this.bizMap[record.biz]) {
-                    continue
-                }
-                this.bizMap[record.biz]
-                    .failFunc(record.bizId, record.msg, record.bizParam)
-                    .then(() => {
-                    })
-                    .catch((e) => {
-                    })
-                    .finally(() => {
-                        this.fireChange(record, 'fail')
-                    })
-            }
+            this.records
+                .filter(r => r.status === 'fail')
+                .forEach((record) => {
+                    changed = true
+                    record.status = 'delete'
+                    if (!this.bizMap[record.biz]) {
+                        return
+                    }
+                    this.bizMap[record.biz]
+                        .failFunc(record.bizId, record.msg, record.bizParam)
+                        .then(() => {
+                        })
+                        .catch((e) => {
+                        })
+                        .finally(() => {
+                            this.fireChange(record, 'fail')
+                        })
+                })
             // delete
             this.records = this.records.filter(r => r.status !== 'delete')
             // sync
@@ -226,7 +241,7 @@ export const taskStore = defineStore("task", {
                 throw new Error('TaskBizNotFound')
             }
             param = Object.assign({
-                timeout: 60 * 1000,
+                timeout: 60 * 10 * 1000,
                 queryInterval: 1 * 1000,
                 status: 'queue',
                 runStart: 0,
@@ -240,8 +255,11 @@ export const taskStore = defineStore("task", {
                 bizParam,
                 runStart: param['runStart'],
                 runAfter: 0,
+                runCalling: false,
                 queryAfter: 0,
                 queryInterval: param['queryInterval'],
+                queryCalling: false,
+                successCalling: false,
                 timeout: param['timeout'],
             } as TaskRecord
             this.records.push(taskRecord)
