21cd60fd5891ff4a185ede736c93164c4d0410fb
ModStart <modstart@163.com>
2024-10-18 16:14:26 +0800

    基础能力优化

---
+++ src/components/common/TaskBizStatus.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import {computed} from "vue";
+import {t} from "../../lang";
+
+interface Props {
+    status: string | undefined,
+    statusMsg: string | undefined,
+}
+
+const props = defineProps<Props>()
+
+const statusColor = computed(() => {
+    const colorMap = {
+        'queue': 'bg-gray-400',
+        'running': 'bg-yellow-500',
+        'success': 'bg-green-500',
+        'fail': 'bg-red-500',
+    }
+    return colorMap[props.status as string] || 'bg-gray-400'
+})
+
+const statusText = computed(() => {
+    const textMap = {
+        'queue': t('排队中'),
+        'running': t('运行中'),
+        'success': t('成功'),
+        'fail': t('失败'),
+    }
+    return textMap[props.status as string] || 'Unknown'
+})
+
+</script>
+
+<template>
+    <div class="text-white px-2 py-1 rounded-full text-sm inline-flex items-center" :class="statusColor">
+        <div class="w-2 h-2 rounded-full bg-white mr-2"></div>
+        <a-tooltip v-if="!!statusMsg" :content="statusMsg">
+            <div>
+                {{ statusText }}
+                <icon-info-circle/>
+            </div>
+        </a-tooltip>
+        <div v-else>{{ statusText }}</div>
+    </div>
+</template>
+
