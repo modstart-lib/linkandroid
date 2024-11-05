f26b93314724a0d736fbde108cd08339d4b3cd08
ModStart <modstart@163.com>
2024-11-05 16:05:17 +0800

    新增：鼠标位置计算通用化

--- electron/mapi/app/lib/position.ts
+++ electron/mapi/app/lib/position.ts
@@ -3,22 +3,44 @@ import {screen} from "electron";
 export const AppPosition = {
     x: 0,
     y: 0,
+    screenWidth: 0,
+    screenHeight: 0,
     id: -1,
-    get(): { x: number; y: number } {
+    get(calculator?: (screenX: number, screenY: number, screenWidth: number, screenHeight: number) => {
+        x: number,
+        y: number
+    }): {
+        x: number;
+        y: number,
+    } {
         const {x, y} = screen.getCursorScreenPoint();
         const currentDisplay = screen.getDisplayNearestPoint({x, y});
         if (AppPosition.id !== currentDisplay.id) {
             AppPosition.id = currentDisplay.id;
-            AppPosition.x = parseInt(
-                String(
-                    currentDisplay.workArea.x + currentDisplay.workArea.width / 2 - 400
-                )
+            AppPosition.screenWidth = currentDisplay.workArea.width;
+            AppPosition.screenHeight = currentDisplay.workArea.height;
+            if (!calculator) {
+                calculator = (
+                    screenX: number,
+                    screenY: number,
+                    screenWidth: number,
+                    screenHeight: number
+                ) => {
+                    console.log('calculator', {screenX, screenY, screenWidth, screenHeight});
+                    return {
+                        x: screenX + screenWidth / 10,
+                        y: screenY + screenHeight / 10,
+                    }
+                }
+            }
+            const res = calculator(
+                currentDisplay.workArea.x,
+                currentDisplay.workArea.y,
+                AppPosition.screenWidth,
+                AppPosition.screenHeight
             );
-            AppPosition.y = parseInt(
-                String(
-                    currentDisplay.workArea.y + currentDisplay.workArea.height / 2 - 200
-                )
-            );
+            AppPosition.x = parseInt(String(res.x));
+            AppPosition.y = parseInt(String(res.y));
         }
         return {
             x: AppPosition.x,
