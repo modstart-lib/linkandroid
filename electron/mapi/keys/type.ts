f0672bf377c537696b0647953e5117f7627192cb
ModStart <modstart@163.com>
2024-11-22 14:34:11 +0800

    新增：快捷键类型

---
+++ electron/mapi/keys/type.ts
@@ -0,0 +1,19 @@
+export enum HotkeyMouseButtonEnum {
+    LEFT = 1,
+    RIGHT = 2
+}
+
+export type HotkeyKeyItem = {
+    key: string
+    altKey: boolean
+    ctrlKey: boolean
+    metaKey: boolean
+    shiftKey: boolean
+    times: number
+}
+
+export type HotkeyMouseItem = {
+    button: HotkeyMouseButtonEnum
+    type: 'click' | 'longPress'
+    clickTimes?: number
+}
