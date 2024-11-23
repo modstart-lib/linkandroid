bccae07a4766be55156325ff44f0fded895063ac
ModStart <modstart@163.com>
2024-11-23 21:39:52 +0800

    优化：空记录组件显示

--- src/components/common/MEmpty.vue
+++ src/components/common/MEmpty.vue
@@ -1,6 +1,12 @@
 <template>
-    <div class="py-10 text-center text-gray-400">
-        <a-empty :title="$t('暂无记录')"/>
+    <div class="py-10 text-center text-gray-400 h-full flex">
+        <div class="m-auto">
+            <div class="text-center">
+                <img class="w-20 w-h-20 object-contain m-auto" src="./../../assets/image/no-record.svg"/>
+            </div>
+            <div class="text-center text-sm">
+                {{ $t('暂无记录') }}
+            </div>
+        </div>
     </div>
 </template>
-
