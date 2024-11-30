47b93911d16c09cf70652786f8bd6f4fb9e8a5fe
ModStart <modstart@163.com>
2024-11-30 20:52:05 +0800

    新增：文件基础名称和后缀获取方法

--- src/lib/file.ts
+++ src/lib/file.ts
@@ -15,4 +15,29 @@ export const FileUtil = {
     blobToFile(blob: Blob, name: string) {
         return new File([blob], name)
     },
+    getExt(path: string) {
+        const ext = path.lastIndexOf('.')
+        if (ext >= 0) {
+            return path.substring(ext + 1)
+        }
+        return ''
+    },
+    getBaseName(path: string, withExt: boolean = false) {
+        // windows
+        if (path.includes('\\')) {
+            path = path.replace(/\\/g, '/')
+        }
+        const last = path.lastIndexOf('/')
+        if (last >= 0) {
+            path = path.substring(last + 1)
+        }
+        if (!withExt) {
+            const ext = path.lastIndexOf('.')
+            if (ext >= 0) {
+                path = path.substring(0, ext)
+            }
+            return path
+        }
+        return path
+    }
 }
