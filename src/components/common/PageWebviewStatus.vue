19bb2cb82a6981de7ba34729fb42d6b61c39c480
ModStart <modstart@163.com>
2024-12-03 15:56:12 +0800

    新增：用户页面状态组件

---
+++ src/components/common/PageWebviewStatus.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+type StatusType = 'success' | 'fail' | 'loading'
+const status = ref<StatusType>('loading')
+const emit = defineEmits([
+    'refresh',
+])
+const setStatus = (s: StatusType) => {
+    status.value = s
+}
+defineExpose({
+    setStatus
+})
+</script>
+
+<template>
+    <div v-if="'loading'===status"
+         class="absolute inset-0 flex bg-white text-white">
+        <div class="m-auto text-center text-gray-400">
+            <div>
+                <icon-loading class="text-3xl"/>
+            </div>
+            <div class="text-sm pt-2">
+                加载中...
+            </div>
+        </div>
+    </div>
+    <div v-else-if="'fail'===status"
+         class="absolute inset-0 flex bg-black bg-opacity-50 text-white">
+        <div class="m-auto text-center text-red-400">
+            <div>
+                <icon-info-circle class="text-3xl"/>
+            </div>
+            <div>
+                内容加载失败，请检查网络连接
+            </div>
+            <div class="mt-4">
+                <a-button size="mini" @click="emit('refresh')">
+                    <template #icon>
+                        <icon-refresh class="tw-mr-1"/>
+                    </template>
+                    刷新
+                </a-button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="less">
+
+</style>
