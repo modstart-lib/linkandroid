cbe8027c1d29a36d6e1f908ed6cc439b34b0d7f2
ModStart <modstart@163.com>
2025-08-06 22:28:27 +0800

    bugfix: missing

--- src/lib/event.ts
+++ src/lib/event.ts
@@ -1,19 +1,18 @@
-import {EventType} from "../types/Event.js";
 import {TinyEmitter} from "tiny-emitter";
 
 const emitter = new TinyEmitter();
 
 export const GlobalEvent = {
-    on: function (event: EventType, callback: Function) {
+    on: function (event: string, callback: Function) {
         emitter.on(event, callback);
     },
-    once: function (event: EventType, callback: Function) {
+    once: function (event: string, callback: Function) {
         emitter.once(event, callback);
     },
-    off: function (event: EventType, callback: Function) {
+    off: function (event: string, callback: Function) {
         emitter.off(event, callback);
     },
-    emit: function (event: EventType, ...args: any[]) {
+    emit: function (event: string, ...args: any[]) {
         emitter.emit(event, ...args);
     },
 };
