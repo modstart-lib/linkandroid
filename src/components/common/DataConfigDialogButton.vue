5a7d3c27a252f0cf71838fd784e3d2aadd646f6d
ModStart <modstart@163.com>
2025-09-10 22:09:28 +0800

    feat: data config button optimized

--- src/components/common/DataConfigDialogButton.vue
+++ src/components/common/DataConfigDialogButton.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import {ref} from "vue";
+import {doCopy} from "./util";
 
 const props = defineProps<{
     title: string;
     name: string;
     defaultValue: string;
     placeholder?: string;
+    param?: Record<string, string>
 }>();
 const visible = ref(false);
 const content = ref("");
@@ -26,7 +28,7 @@ const doRestore = () => {
 <template>
     <a-button @click="doShow()">
         <template #icon>
-            <icon-settings />
+            <icon-settings/>
         </template>
         {{ title }}
     </a-button>
@@ -44,7 +46,22 @@ const doRestore = () => {
         </template>
         <div style="max-height: 60vh">
             <div>
-                <a-textarea v-model="content" :placeholder="placeholder" :auto-size="{minRows: 15, maxRows: 15}" />
+                <a-textarea v-model="content" :placeholder="placeholder" :auto-size="{minRows: 15, maxRows: 15}"/>
+            </div>
+            <div v-if="props.param">
+                <div class="mt-2 font-bold">{{ $t("可用变量") }}:</div>
+                <div class="mt-1">
+                    <div v-for="(value, key, index) in props.param" :key="key"
+                         class="mr-4 inline-flex items-center text-xs">
+                        <div class="font-mono mr-1 cursor-pointer"
+                             @click="doCopy(`{${key}}`)">
+                            {{ "{" + key + "}" }}
+                        </div>
+                        <div class="text-gray-400">
+                            {{ value }}
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </a-modal>
